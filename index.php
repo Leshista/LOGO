786360db985d8a7aa0484954dd055867abfae90a
leshista <a.leshista@gmail.com>
2022-05-30 15:10:09 +0300

    Included Bootstrap cdn, normalize css and added a review

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>LOGO</title>
 </head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/style.css" />
 
 <body>
+    <header class="header"></header>
+    <main class="main">
+    </main>
+    <footer class="footer"></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
 </body>
 
