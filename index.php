556e6248ecad831118b3d52c75cb3a6477d8705d
leshista <a.leshista@gmail.com>
2022-05-30 17:11:24 +0300

    Main-top done

--- index.php
+++ index.php
@@ -12,60 +12,68 @@
 
 <body>
     <header class="header">
-        <div class="wrapper">
-            <div class="header-top">
-                <div class="header-top__wrapper">
-                    <div class="header-top__left">
-                        <div class="logo">
-                            <img src="img/LOGO.png" alt="logo">
+        <div class="header-top">
+            <div class="header-top__wrapper">
+                <div class="header-top__left">
+                    <div class="logo">
+                        <img src="img/LOGO.png" alt="logo">
+                    </div>
+                    <div class="adress">
+                        <div class="vector">
+                            <img src="img/Vector.png" alt="vector">
                         </div>
-                        <div class="adress">
-                            <div class="vector">
-                                <img src="img/Vector.png" alt="vector">
-                            </div>
-                            <div class="adress__text">
-                                <p class="city">Ростов-на-Дону</p>
-                                <p class="street">ул. Ленина 2Б</p>
-                            </div>
+                        <div class="adress__text">
+                            <p class="city">Ростов-на-Дону</p>
+                            <p class="street">ул. Ленина 2Б</p>
                         </div>
                     </div>
-                    <div class="header-top__right">
-                        <div class="whatsapp">
-                            <div class="whatsapp__logo">
-                                <img src="img/whatsapp.png" alt="WhatsApp Logo">
-                            </div>
-                            <p class="whatsapp__number">+7(863) 000 00 00</p>
+                </div>
+                <div class="header-top__right">
+                    <div class="whatsapp">
+                        <div class="whatsapp__logo">
+                            <img src="img/whatsapp.png" alt="WhatsApp Logo">
                         </div>
-                        <div class="sign-up">
-                            <button class="sign-up__button btn">Записаться на приём</button>
-                        </div>
+                        <p class="whatsapp__number">+7(863) 000 00 00</p>
+                    </div>
+                    <div class="sign-up">
+                        <button class="sign-up__button btn">Записаться на приём</button>
                     </div>
                 </div>
             </div>
-            <div class="header-bottom">
-                <div class="header-bottom__wrapper">
-                    <nav class="navigation">
-                        <ul class="navigation__list">
-                            <li class="navigation__item">О клинике</li>
-                            <li class="navigation__item">Услуги</li>
-                            <li class="navigation__item">Специалисты</li>
-                            <li class="navigation__item">Цены</li>
-                            <li class="navigation__item">Контакты</li>
-                        </ul>
-                    </nav>
-                </div>
+        </div>
+        <div class="header-bottom">
+            <div class="header-bottom__wrapper">
+                <nav class="navigation">
+                    <ul class="navigation__list">
+                        <li class="navigation__item">О клинике</li>
+                        <li class="navigation__item">Услуги</li>
+                        <li class="navigation__item">Специалисты</li>
+                        <li class="navigation__item">Цены</li>
+                        <li class="navigation__item">Контакты</li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </header>
     <main class="main">
-        <div class="wrapper main__wrapper">
+        <div class="main-top">
+            <section class="main-top__left">
+                <div class="main-textblock">
+                    <p class="main-textblock__title">Многопрофильная клиника для детей
+                        и взрослых</p>
+                    <p class="main-textblock__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </div>
+            </section>
+            <section class="main-top__right">
+                <img src="img/photo1.png" alt="Photo of our clinic">
+            </section>
+        </div>
+        <div class="main-bottom">
 
         </div>
     </main>
     <footer class="footer">
-        <div class="wrapper footer__wrapper">
 
-        </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
