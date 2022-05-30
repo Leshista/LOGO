96bfb6a4dfd4d71b5ff703f46b17d01879fde15a
leshista <a.leshista@gmail.com>
2022-05-30 16:31:25 +0300

    Header-top complete

--- index.php
+++ index.php
@@ -11,10 +11,50 @@
 <link rel="stylesheet" href="css/style.css" />
 
 <body>
-    <header class="header"></header>
+    <header class="header">
+        <div class="wrapper">
+            <div class="header-top">
+                <div class="header-top__left">
+                    <div class="logo">
+                        <img src="img/LOGO.png" alt="logo">
+                    </div>
+                    <div class="adress">
+                        <div class="vector">
+                            <img src="img/Vector.png" alt="vector">
+                        </div>
+                        <div class="adress__text">
+                            <p class="city">Ростов-на-Дону</p>
+                            <p class="street">ул. Ленина 2Б</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-top__right">
+                    <div class="whatsapp">
+                        <div class="whatsapp__logo">
+                            <img src="img/whatsapp.png" alt="WhatsApp Logo">
+                        </div>
+                        <p class="whatsapp__number">+7(863) 000 00 00</p>
+                    </div>
+                    <div class="sign-up">
+                        <button class="sign-up__button btn">Записаться на приём</button>
+                    </div>
+                </div>
+            </div>
+            <div class="header__bottom">
+
+            </div>
+        </div>
+    </header>
     <main class="main">
+        <div class="wrapper main__wrapper">
+
+        </div>
     </main>
-    <footer class="footer"></footer>
+    <footer class="footer">
+        <div class="wrapper footer__wrapper">
+
+        </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="js/script.js"></script>
 </body>
