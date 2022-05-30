fd8cfce230a9a811ec651cce4fbe5d5165d486ab
leshista <a.leshista@gmail.com>
2022-05-30 16:47:18 +0300

    Header is done

--- index.php
+++ index.php
@@ -14,34 +14,46 @@
     <header class="header">
         <div class="wrapper">
             <div class="header-top">
-                <div class="header-top__left">
-                    <div class="logo">
-                        <img src="img/LOGO.png" alt="logo">
-                    </div>
-                    <div class="adress">
-                        <div class="vector">
-                            <img src="img/Vector.png" alt="vector">
+                <div class="header-top__wrapper">
+                    <div class="header-top__left">
+                        <div class="logo">
+                            <img src="img/LOGO.png" alt="logo">
                         </div>
-                        <div class="adress__text">
-                            <p class="city">Ростов-на-Дону</p>
-                            <p class="street">ул. Ленина 2Б</p>
+                        <div class="adress">
+                            <div class="vector">
+                                <img src="img/Vector.png" alt="vector">
+                            </div>
+                            <div class="adress__text">
+                                <p class="city">Ростов-на-Дону</p>
+                                <p class="street">ул. Ленина 2Б</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="header-top__right">
-                    <div class="whatsapp">
-                        <div class="whatsapp__logo">
-                            <img src="img/whatsapp.png" alt="WhatsApp Logo">
+                    <div class="header-top__right">
+                        <div class="whatsapp">
+                            <div class="whatsapp__logo">
+                                <img src="img/whatsapp.png" alt="WhatsApp Logo">
+                            </div>
+                            <p class="whatsapp__number">+7(863) 000 00 00</p>
                         </div>
-                        <p class="whatsapp__number">+7(863) 000 00 00</p>
-                    </div>
-                    <div class="sign-up">
-                        <button class="sign-up__button btn">Записаться на приём</button>
+                        <div class="sign-up">
+                            <button class="sign-up__button btn">Записаться на приём</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="header__bottom">
-
+            <div class="header-bottom">
+                <div class="header-bottom__wrapper">
+                    <nav class="navigation">
+                        <ul class="navigation__list">
+                            <li class="navigation__item">О клинике</li>
+                            <li class="navigation__item">Услуги</li>
+                            <li class="navigation__item">Специалисты</li>
+                            <li class="navigation__item">Цены</li>
+                            <li class="navigation__item">Контакты</li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </header>
