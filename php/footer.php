cc89f323779156ea109218ed5fc8ed6a632dc8e7
leshista <a.leshista@gmail.com>
2022-06-02 16:24:25 +0300

    Started to work on mobile version + php refactoring

---
+++ php/footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div class="footer__wrapper">
+        <div class="footer__logo"><img src="img/white LOGO.png" alt="LOGO"></div>
+        <nav class="footer__nav">
+            <div class="footer__list">
+                <div class="footer__item">О клинике</div>
+                <div class="footer__item">Услуги</div>
+                <div class="footer__item">Специалисты</div>
+                <div class="footer__item">Цены</div>
+                <div class="footer__item">Контакты</div>
+            </div>
+        </nav>
+        <div class="socials">
+            <div class="socials__instagram"><a href="/"><img src="img/instagram.png" alt="instargam link"></a></div>
+            <div class="socials__whatsapp"><a href="/"><img src="img/whatsapp2.png" alt="whatsapp link"></a></div>
+            <div class="socials__telegram"><a href="/"><img src="img/telegram.png" alt="telergam link"></a></div>
+        </div>
+    </div>
+</footer>
---
+++ php/main.php
@@ -0,0 +1,137 @@
+<main class="main">
+    <div class="main-top">
+        <section class="main-top__left">
+            <div class="main-textblock">
+                <p class="main-textblock__title">Многопрофильная клиника для детей
+                    и взрослых</p>
+                <p class="main-textblock__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            </div>
+        </section>
+        <section class="main-top__right">
+            <img src="img/photo1.png" alt="A photo of our clinic" />
+        </section>
+    </div>
+    <div class="main-bottom">
+        <div class="main-bottom__wrapper">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-interval="0">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="check-up">
+                            <div class="check-up-left">
+                                <div class="check-up-left__wrapper">
+                                    <p class="check-up__title">CHECK-UP</p>
+                                    <p class="check-up__text">для мужчин</p>
+                                    <ul class="check-up__list">
+                                        <li class="check-up__item">Гормональный скрининг</li>
+                                        <li class="check-up__item">Тестостерон</li>
+                                        <li class="check-up__item">Свободный тестостерон</li>
+                                        <li class="check-up__item">Глобулин, связывающий половые гормоны</li>
+                                    </ul>
+                                    <div class="price">
+                                        <p class="price__now">Всего 2800₽</p>
+                                        <p class="price__then">3500₽</p>
+                                    </div>
+                                    <div class="check-up__buttons">
+                                        <button class="check-up__sign-in">Записаться</button>
+                                        <button class="check-up__more">Подробнее</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="check-up-right">
+                                <img src="img/checkup.png" alt="Photo of a doctor">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="check-up">
+                            <div class="check-up-left">
+                                <div class="check-up-left__wrapper">
+                                    <p class="check-up__title">CHECK-UP</p>
+                                    <p class="check-up__text">для мужчин</p>
+                                    <ul class="check-up__list">
+                                        <li class="check-up__item">Гормональный скрининг</li>
+                                        <li class="check-up__item">Тестостерон</li>
+                                        <li class="check-up__item">Свободный тестостерон</li>
+                                        <li class="check-up__item">Глобулин, связывающий половые гормоны</li>
+                                    </ul>
+                                    <div class="price">
+                                        <p class="price__now">Всего 2800₽</p>
+                                        <p class="price__then">3500₽</p>
+                                    </div>
+                                    <div class="check-up__buttons">
+                                        <button class="check-up__sign-in">Записаться</button>
+                                        <button class="check-up__more">Подробнее</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="check-up-right">
+                                <img src="img/checkup.png" alt="Photo of a doctor">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="check-up">
+                            <div class="check-up-left">
+                                <div class="check-up-left__wrapper">
+                                    <p class="check-up__title">CHECK-UP</p>
+                                    <p class="check-up__text">для мужчин</p>
+                                    <ul class="check-up__list">
+                                        <li class="check-up__item">Гормональный скрининг</li>
+                                        <li class="check-up__item">Тестостерон</li>
+                                        <li class="check-up__item">Свободный тестостерон</li>
+                                        <li class="check-up__item">Глобулин, связывающий половые гормоны</li>
+                                    </ul>
+                                    <div class="price">
+                                        <p class="price__now">Всего 2800₽</p>
+                                        <p class="price__then">3500₽</p>
+                                    </div>
+                                    <div class="check-up__buttons">
+                                        <button class="check-up__sign-in">Записаться</button>
+                                        <button class="check-up__more">Подробнее</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="check-up-right">
+                                <img src="img/checkup.png" alt="Photo of a doctor">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="check-up">
+                            <div class="check-up-left">
+                                <div class="check-up-left__wrapper">
+                                    <p class="check-up__title">CHECK-UP</p>
+                                    <p class="check-up__text">для мужчин</p>
+                                    <ul class="check-up__list">
+                                        <li class="check-up__item">Гормональный скрининг</li>
+                                        <li class="check-up__item">Тестостерон</li>
+                                        <li class="check-up__item">Свободный тестостерон</li>
+                                        <li class="check-up__item">Глобулин, связывающий половые гормоны</li>
+                                    </ul>
+                                    <div class="price">
+                                        <p class="price__now">Всего 2800₽</p>
+                                        <p class="price__then">3500₽</p>
+                                    </div>
+                                    <div class="check-up__buttons">
+                                        <button class="check-up__sign-in">Записаться</button>
+                                        <button class="check-up__more">Подробнее</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="check-up-right">
+                                <img src="img/checkup.png" alt="Photo of a doctor">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="controlling-panel">
+        <a class="slide-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev"><img src="img/arrow-left.png" alt="arrow-left"></a>
+        <p class="controlling-panel__numbers"><span class="slide-index">1</span>/4</p><a class="slide-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next"><img src="img/arrow-right.png" alt="arrow-right"></a>
+    </div>
+    <!--  -->
+
+    <!--  -->
+</main>
