59ff5e57a4545650a126adcef31cc17f3bdfe807
leshista <a.leshista@gmail.com>
2022-06-02 16:51:03 +0300

    Mobile version almost done (Hamburger menu remains)

---
+++ php/header-mobile.php
@@ -0,0 +1,14 @@
+<header class="header-mobile">
+    <div class="burger-menu">
+        <div class="burger-menu__stripe"><img src="img/stripe.png" alt="a burger stripe"></div>
+        <div class="burger-menu__stripe"><img src="img/stripe.png" alt="a burger stripe"></div>
+        <div class="burger-menu__stripe"><img src="img/stripe.png" alt="a burger stripe"></div>
+    </div>
+    <div class="mobile-logo">
+        <img src="img/LOGOmob.png" alt="LOGO">
+    </div>
+    <div class="mobile-info">
+        <p class="mobile-info__number">+7(863) 000 00 00</p>
+        <p class="mobile-info__address">Ростов-на-Дону</p>
+    </div>
+</header>
